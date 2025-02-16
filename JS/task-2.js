69a1a55b7fee330ac3640e96807569f0abdfeeca
Mykyta-Veklich <nikitaveklich01@gmail.com>
2025-02-16 16:18:42 +0200

    Code update (JS)
    Added task files for practice.

---
+++ JS/task-2.js
@@ -0,0 +1,9 @@
+const total = 100;
+const ordered = 0;
+
+if (total > ordered) {
+    console.log("Замовлення оформлено, з вами зв'яжеться менеджер.");
+}
+else {
+    console.log("На складі недостатньо товарів!");
+}
