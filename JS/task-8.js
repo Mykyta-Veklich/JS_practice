3834f571f5c1e0dbcf95f77d93faea82e3d05bda
Mykyta-Veklich <nikitaveklich01@gmail.com>
2025-02-19 18:53:12 +0200

    Code update (JS)
    Task-7, task-8, task-9 done.

---
+++ JS/task-8.js
@@ -0,0 +1,7 @@
+// const fruits = ['apple', 'plum', 'pear', 'orange', 'banana'];
+// const firstTwoEls = fruits.slice(0, 2);
+// const nonExtremeEls = fruits.slice(1, 4);
+// const lastThreeEls = fruits.slice(2, 5);
+// console.log(firstTwoEls);  
+// console.log(nonExtremeEls); 
+// console.log(lastThreeEls);
